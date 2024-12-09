4b96e8483c67d3d942051617352c2d014e7545bc
Gabriel Gatimu <geraldg652@gmail.com>
2024-12-09 11:33:04 +0300

    finished part 2of day3

--- day3/mull_over.go
+++ day3/mull_over.go
@@ -8,6 +8,8 @@ import (
 	"strconv"
 )
 
+var mulPattern = `mul\((\d{1,3}),(\d{1,3})\)`
+
 func getInput(filePath string) string {
 	content, err := os.ReadFile(filePath)
 	if err != nil {
@@ -17,14 +19,11 @@ func getInput(filePath string) string {
 	return string(content)
 }
 
-func main() {
-	input := getInput("input.txt")
-
-	mulPattern := `mul\((\d{1,3}),(\d{1,3})\)`
+// part 1
+func getMulValues(input string) int {
+	sum := 0
 	mulRegex := regexp.MustCompile(mulPattern)
 	matches := mulRegex.FindAllStringSubmatch(input, -1)
-
-	sum := 0
 
 	for _, mul := range matches {
 		x, _ := strconv.Atoi(mul[1])
@@ -32,5 +31,34 @@ func main() {
 		sum += x * y
 	}
 
-	fmt.Println(sum)
+	return sum
+}
+
+// part 2
+func getEnabledValues(input string) int {
+	sum := 0
+	do := true
+
+	doRegex := regexp.MustCompile(`mul\(\d+,\d+\)|do\(\)|don't\(\)`)
+	matches := doRegex.FindAllString(input, -1)
+
+	for _, instruction := range matches {
+		//fmt.Println("instruction", instruction)
+
+		switch {
+		case instruction == "do()":
+			do = true
+		case do && regexp.MustCompile(mulPattern).MatchString(instruction):
+			sum += getMulValues(instruction)
+		default:
+			do = false
+		}
+	}
+	return sum
+}
+
+func main() {
+	input := getInput("input.txt")
+	fmt.Println("Part1: ", getMulValues(input))
+	fmt.Println("Part2: ", getEnabledValues(input))
 }
